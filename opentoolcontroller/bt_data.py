53cfdeec0d977e05d91a07b975782a96455ca534
Shawn Wright <399wright@gmail.com>
2023-11-27 21:10:56 -0500

    hal layer fixes

--- opentoolcontroller/bt_data.py
+++ opentoolcontroller/bt_data.py
@@ -810,7 +810,8 @@ class SetNode(Node):
                         tool_model.setData(child.setIndex().siblingAtColumn(col.VALUE), value)
 
                     elif type_info == typ.D_OUT_NODE:
-                        child.halQueuePut(value)
+                        value = bool(child.value)
+                        child.setIndex().internalPointer().halQueuePut(value)
 
                     elif type_info == typ.A_OUT_NODE:
                         pass #FIXME TODO HAL
