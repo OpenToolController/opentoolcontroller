964a7f39c3dfbce7ec32555c43f03b9cc22f707e
Shawn Wright <399wright@gmail.com>
2024-03-04 21:06:18 -0500

    adding multi readers

--- opentoolcontroller/main.py
+++ opentoolcontroller/main.py
@@ -71,7 +71,7 @@ class Window(QtWidgets.QMainWindow):
         #tick_rate_ms = self.tool_model.data(tool_index.siblingAtColumn(col.TICK_RATE_MS), QtCore.Qt.DisplayRole)
 
 
-        reader_periods = [100,200,500]
+        reader_periods = [50,100,200]
         self.reader_group = HalReaderGroup(reader_periods)
 
         self.behavior_runners = []
