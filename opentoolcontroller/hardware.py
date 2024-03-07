d428408a01e1dc1eb5480a106672bc0446e0a1b0
Shawn Wright <399wright@gmail.com>
2024-03-06 21:33:19 -0500

    multi streamers

--- opentoolcontroller/hardware.py
+++ opentoolcontroller/hardware.py
@@ -259,7 +259,7 @@ class HalReader():
 
         #node.setSamplerPins(node_sampler_pins) # This is a list of indexes
         subprocess.call(['halcmd', 'setp', 'sampler.'+str(sampler_number)+'.enable', 'True'])
-        subprocess.call(['halcmd', 'addf', 'sampler.'+str(sampler_number), 'gui_'+str(sampler_number)])
+        subprocess.call(['halcmd', 'addf', 'sampler.'+str(sampler_number), 'gui_'+str(sampler_number+1)])
 
 
         # Sampler userspace component, stdbuf fixes bufering issue
@@ -287,7 +287,7 @@ class HalReader():
 
         #node.setSamplerPins(node_sampler_pins) # This is a list of indexes
         subprocess.call(['halcmd', 'setp', 'streamer.'+str(streamer_number)+'.enable', 'True'])
-        subprocess.call(['halcmd', 'addf', 'streamer.'+str(streamer_number), 'gui_'+str(sampler_number)])
+        subprocess.call(['halcmd', 'addf', 'streamer.'+str(streamer_number), 'gui_'+str(sampler_number+1)])
 
         # Streamer userspace component, stdbuf fixes bufering issue
         self.p_streamer = subprocess.Popen(['halcmd', 'loadusr', 'halstreamer', '-c', str(streamer_number)], 
